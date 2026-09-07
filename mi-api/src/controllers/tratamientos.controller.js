1d97b67c1e346f40bbc15086170d249293f2def1
BraynerBarragann <brayner.barrragann@gmail.com>
2026-09-06 23:22:51 -0500

    Estructura tratamiento

---
+++ mi-api/src/controllers/tratamientos.controller.js
@@ -0,0 +1,52 @@
+let tratamientos = [
+    {
+    id: 1,
+    usuario_id: 1,
+    nombre: 'tratamiento1 ',
+    descripcion: '21322'
+    },
+
+    {
+    id: 2,
+    usuario_id: 1,
+    nombre: 'tratamiento2',
+    descripcion: '32345'
+    },
+    {
+    id: 3,
+    usuario_id: 2,
+    nombre: 'tratamiento3',
+    descripcion: '32345'
+    }
+
+];
+
+const getAll = (req, res) => {
+
+  const usuarioId = req.params.usuarioId;
+
+  const resultado = tratamientos.filter(
+    t => t.usuario_id == usuarioId
+  );
+
+  res.json({
+    ok: true,
+    data: resultado
+  });
+};
+
+const getById = (req, res) => {
+  const item = tratamientos.find(
+    t => t.id == req.params.id
+  );
+  if (!item) return res.status(404)
+    .json({ ok: false, msg: 'No encontrado' });
+  res.json({ ok: true, data: item });
+};
+
+const create = (req, res) => {
+  const nuevo = { id: Date.now(), ...req.body };
+  tratamientos.push(nuevo);
+  res.status(201).json({ ok: true, data: nuevo });
+};
+module.exports = { getAll, getById, create };
--- mi-api/src/routes/usuarios.routes.js
+++ mi-api/src/routes/usuarios.routes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/usuarios.controller');
+const tratamientosctrl = require('../controllers/tratamientos.controller');
 
 router.get('/', ctrl.getAll);
 router.get('/:id', ctrl.getById);
 router.post('/', ctrl.create);
+
+router.get('/:usuarioId/tratamientos', tratamientosctrl.getAll);
+router.get('/:usuarioId/tratamientos/:id', tratamientosctrl.getById);
+
+
+
 
 module.exports = router;
