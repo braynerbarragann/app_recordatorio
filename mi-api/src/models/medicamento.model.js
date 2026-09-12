e1bf850c5725a436fca1394fa9d803e458b73a7e
kevyncorrea1604 <kevyncorrea1604@gmail.com>
2026-09-12 09:29:03 -0500

    Kevyn: Se realizaron correciines dentro de models de via_administracion y medicamentos

--- mi-api/src/models/medicamento.model.js
+++ mi-api/src/models/medicamento.model.js
@@ -17,50 +17,8 @@ const getById = async (id) => {
     return rows[0];
 };
 
-const getByUsuarioId = async (usuarioId) => {
-    const [rows] = await pool.query(
-        'SELECT * FROM medicamento WHERE usuario_id = ? ORDER BY id DESC',
-        [usuarioId]
-    );
-
-    return rows;
-};
-
-const create = async (data) => {
-    const {
-        usuario_id,
-        nombre,
-        presentacion,
-        concentracion,
-        descripcion
-    } = data;
-
-    const [result] = await pool.query(
-        `INSERT INTO medicamento
-        (usuario_id, nombre, presentacion, concentracion, descripcion)
-        VALUES (?, ?, ?, ?, ?)`,
-        [
-            usuario_id,
-            nombre,
-            presentacion,
-            concentracion,
-            descripcion
-        ]
-    );
-
-    return {
-        id: result.insertId,
-        usuario_id,
-        nombre,
-        presentacion,
-        concentracion,
-        descripcion
-    };
-};
 
 module.exports = {
     getAll,
     getById,
-    getByUsuarioId,
-    create
 };
--- mi-api/src/models/via_administracion.model.js
+++ mi-api/src/models/via_administracion.model.js
@@ -17,44 +17,7 @@ const getById = async (id) => {
     return rows[0];
 };
 
-const getByUsuarioId = async (usuarioId) => {
-    const [rows] = await pool.query(
-        'SELECT * FROM via_administracion WHERE usuario_id = ? ORDER BY id DESC',
-        [usuarioId]
-    );
-
-    return rows;
-};
-
-const create = async (data) => {
-    const {
-        usuario_id,
-        nombre,
-        descripcion
-    } = data;
-
-    const [result] = await pool.query(
-        `INSERT INTO via_administracion
-        (usuario_id, nombre, descripcion)
-        VALUES (?, ?)`,
-        [
-            usuario_id,
-            nombre,
-            descripcion
-        ]
-    );
-
-    return {
-        id: result.insertId,
-        usuario_id,
-        nombre,
-        descripcion
-    };
-};
-
 module.exports = {
     getAll,
     getById,
-    getByUsuarioId,
-    create
 };
